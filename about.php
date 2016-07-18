4de38b50caa2dc0da950b3a7393fbad1f0699b09
Lacey Guthrie <lacey.guthrie@gmail.com>
2016-07-18 13:19:15 -0400

    final styles

--- about.php
+++ about.php
@@ -1,9 +1,28 @@
 <?php include( 'partials/header.php' ); ?>
 
 <section class="about">
-
-  
-
+  <section class="container">
+    <h2>About</h2>
+    <section class="inner-container">
+      <div class="about-image-header">
+      </div>
+      <div class="row about-summary">
+        <div class="medium-12 columns">
+          <h3>Your Own Personal &amp; Professional Consultant<br />for the Past 30 Years</h3>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        </div>
+      </div>
+      <div class="row block-row meet-chuck">
+        <div class="small-12 medium-6 columns chuck-portrait">
+        </div>
+        <div class="small-12 medium-6 columns chuck-bio">
+          <h4>Chuck Luley</h4>
+          <p class="chuck-title">President</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Duis mollis, est non commodo luctus, nisi erat porttitor ligula. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor.</p>
+        </div>
+      </div>
+    </section>
+  </section>
 </section>
 
 <?php include( 'partials/footer.php' ); ?>
